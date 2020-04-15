ae5bea0faad1a6c9316794702e65e24c4564dc65
ELHOR Youssef <elhor.yyoussef@hotmail.com>
2020-04-15 09:22:47 +0200

    Functional test
    The test method for the about us page has been created

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -19,4 +19,18 @@ class HomeControllerTest extends WebTestCase
         // On vérifie si le message ci dessous est présent dans un paragraphe
         $this->assertSelectorTextContains('p', 'Create and manage new materials');
     }
+
+    /* Test sur la page About Us */
+    public function testAboutUs(){
+        // Création d'un client
+        $client = static::createClient();
+        // Le client fait une requete
+        $client->request('GET', '/aboutUs');
+        // On vérifie si la requete HTTP est OK
+        $this->assertResponseIsSuccessful();
+        // On vérifie si le message ci dessous est présent dans le titre principal de la page
+        $this->assertSelectorTextContains('h1', 'About us');
+        // On vérifie si le message ci dessous est présent dans un paragraphe
+        $this->assertSelectorTextContains('p', 'We are the only company in the world that provides to clients the possibility to create and manage their own materials');
+    }
 }
