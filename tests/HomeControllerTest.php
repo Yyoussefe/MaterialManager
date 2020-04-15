72d15eea2113b051b394edd1e979f0d835f625a6
ELHOR Youssef <elhor.yyoussef@hotmail.com>
2020-04-15 09:22:10 +0200

    Functional test
    The test method for the home page has been created

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -6,12 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testSomething()
+    /* Test sur la page Index*/
+    public function testIndex()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-
+        // Le client fait une requete
+        $client->request('GET', '/home');
+        // On vérifie si la requete HTTP est OK
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        // On vérifie si le message ci dessous est présent dans le titre principal de la page
+        $this->assertSelectorTextContains('h1', 'Welcome to Material Manager plateform');
+        // On vérifie si le message ci dessous est présent dans un paragraphe
+        $this->assertSelectorTextContains('p', 'Create and manage new materials');
     }
 }
